1314f56d60f980817d8c7e19c527ca65ce4ee5d3
dionisioR <72356766+dionisioR@users.noreply.github.com>
2024-12-24 12:45:27 -0300

    Update 2024-12-24   12h45
    Update 2024-12-24   12h45

---
+++ Models/Smartphone.cs
@@ -0,0 +1,39 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string _modelo;
+        private string _imei;
+        private int _memoria;
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+            _modelo = modelo;
+            _imei = imei;
+            _memoria = memoria;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+          public void Dados()
+        {
+            Console.WriteLine($"Número: {Numero}");
+            Console.WriteLine($"Modelo: {_modelo}");
+            Console.WriteLine($"IMEI: {_imei}");
+            Console.WriteLine($"Memória: {_memoria}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("=====================================");
+
+Nokia n = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("Whatsapp");
+n.Dados();
+Console.WriteLine("=====================================");
+
+Iphone i = new Iphone("987654321", "Iphone 12", "987654321", 128);
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo("Instagram");
+i.Dados();
